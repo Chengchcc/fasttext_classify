d3555c9928678e005db8ec084da4c59186ac92ed
Chengchcc <chen@aion.network>
2020-04-24 15:22:23 +0800

    update order struct

--- mock_data.py
+++ mock_data.py
@@ -1,11 +1,11 @@
-import time
+import time, re
 from elasticsearch import helpers
 from utils import connect_elasticserach
 file_path='./data/cooking.train'
 host="139.9.250.148"
 port=9200
-username="XXX"
-password="XXX"
+username="recsys_admin"
+password="PLAT4life"
 
 def gen():
     es = connect_elasticserach(host,port, username, password)
@@ -16,23 +16,18 @@ def gen():
         name = ""
         prefix_len = len("__label__")
         for data in f.readlines():
-            line = data.split(" ")
+            line = re.sub(r'[^\w\s]', '', data).strip().lower().split(" ")
             for buffer in line:
                 if buffer.startswith("__label__"):
                     label.append(buffer[prefix_len:])
                 else:
                     name += (" "+ buffer)
-            if len(label) < 3:
-                label.extend(["null" for x in range(3 - len(label))])
             action = {
                 "_index": "recsys",
-                "_type": "goodsdata",
                 "_source": {
-                    "ITEM": name,
-                    "PTY_1": label[0],
-                    "PTY_2": label[1],
-                    "PTY_3": label[2],
-                    "@timestmap": int(round(time.time()*1000))
+                    "desc": name,
+                    "tags":list(label),
+                    "orderId": i,
                 },
             }
             label.clear()
--- recsys.py
+++ recsys.py
@@ -1,10 +1,16 @@
 import re, json ,time, fasttext
 from utils import load_yaml_confg, connect_elasticserach, load_fasttext_model
 
-def fastext_train(file, output):
+old_precision = 0
+
+def fastext_train(train_file, valid_file, output):
     model = fasttext.train_supervised(
-        input=file, lr=1.0, label='__label__', epoch=25, wordNgrams=2, dim=200, bucket=200000, loss='hs')
-    model.save_model(output)
+        input=train_file, lr=1.0, label='__label__', epoch=25, wordNgrams=2, dim=200, bucket=200000, loss='hs')
+    res = model.test(valid_file)
+    global old_precision
+    if res[1] > old_precision:
+        old_precision = res[1]
+        model.save_model(output)
     return model
 
 
@@ -13,7 +19,8 @@ def train_job():
     config = load_yaml_confg("./conf.yml")
     # model
     fasttext_model = config['model']['fasttext_model']
-    tmp_data_path = config['model']['tmp_data_path']
+    train_data_path = config['model']['train_data_path']
+    valid_data_path = config['model']['valid_data_path']
     # data
     es_host = config["data"]["es_host"]
     es_port = config["data"]["es_port"]
@@ -24,26 +31,37 @@ def train_job():
     es = connect_elasticserach(es_host, es_port, es_username, es_password)
 
     query_body = {
-        "query": {"range": {
-            "@timestmap": {
-                "gte": 10
+        "query": {
+            "function_score": {
+                "query": {
+                    "match_all": {}
+                },
+                "script_score" : {
+                    "script" : {
+                        "source": "Math.log(2 + doc['orderId'].value)"
+                    }
+                }
             }
-        }},
-        "size":  12400
+        },
+        "size":10000
     }
 
-    res = es.search(body=query_body, index="recsys", doc_type='goodsdata')
-    with open(tmp_data_path, 'wt') as f:
-        for data in res['hits']['hits']:
+    res = es.search(body=query_body, index="recsys")
+    with open(train_data_path, 'wt') as ft, open(valid_data_path, 'wt') as fv:
+        count = len(res['hits']['hits'])
+
+        for idx, data in enumerate(res['hits']['hits']):
             source = data['_source']
-            labels = [source['PTY_1'], source['PTY_2'], source['PTY_3']]
-            if 'null' in labels:
-                labels.remove('null')
+            labels = list(source['tags'])
             labels_str = ' '.join(['__label__'+label for label in labels])
-            content = source['ITEM']
-            f.write(labels_str+' '+re.sub(r'[^\w\s]', '', content).strip().lower()+'\n')
-    f.close()
-    fastext_train(tmp_data_path, fasttext_model)
+            content = source['desc']
+            if idx< count*0.8:
+                ft.write(labels_str+' '+re.sub(r'[^\w\s]', '', content).strip().lower()+'\n')
+            else:
+                fv.write(labels_str+' '+re.sub(r'[^\w\s]', '', content).strip().lower()+'\n')
+    ft.close()
+    fv.close()
+    fastext_train(train_data_path, valid_data_path, fasttext_model)
 
 if __name__ == "__main__":
     train_job()
